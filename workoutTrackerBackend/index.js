8058de8294a11d80c4c0218e7645a7e5a0be80a6
elijahgott <gottelijah@gmail.com>
2025-09-11 09:39:45 -0500

    added rest api functionality (get all, get one, delete, add) for users

--- workoutTrackerBackend/index.js
+++ workoutTrackerBackend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const morgan  = require('morgan')
 const app = express()
+app.use(express.json())
+app.use(morgan('tiny'))
 
-const users = [
+let users = [
   {
     username: 'elo',
+    id: '1',
     workouts: [
       {
         name: 'push',
@@ -43,6 +47,7 @@ const users = [
   },
   {
     username: 'judith',
+    id: '2',
     workouts: [
       {
         name: 'push',
@@ -65,8 +70,54 @@ const users = [
   }
 ]
 
+const generateId = () => {
+  const maxId = users.length > 0 ? Math.max(...users.map(u => Number(u.id))) : 0
+
+  return String(maxId + 1)
+}
+
+// get all users
 app.get('/api/users', (req, res) => {
   res.json(users)
+})
+
+// get specific user
+app.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  const user = users.find(u => u.id === id)
+  console.log(user)
+  if(user){
+    res.json(user)
+  }
+  else{
+    res.status(404).end()
+  }
+})
+
+// delete specific user
+app.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  users = users.filter(u => u.id !== id)
+
+  res.status(204).end()
+})
+
+// create new user
+app.post('/api/users', (req, res) => {
+  const body = req.body
+  if(!body.username){
+    return res.status(400).json({error: 'missing username'})
+  }
+
+  const user = {
+    username: body.username,
+    workouts: body.workouts || [],
+    id: generateId()
+  }
+
+  users = users.concat(user)
+
+  res.json(user)
 })
 
 const PORT = 3001
