808eebdcbbdcc7c4bee73180e399461546673033
elijahgott <gottelijah@gmail.com>
2025-09-13 10:05:00 -0500

    refactored backend

---
+++ workoutTrackerBackend/controllers/users.js
@@ -0,0 +1,84 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+// get all users
+usersRouter.get('/', (req, res) => {
+  User.find({})
+    .then((users) => res.json(users))
+    .catch((e) => {
+      console.log('Unable to fetch users: ', e)
+      res.status(404).end()
+    })
+})
+
+// get specific user
+usersRouter.get('/:id', (req, res) => {
+  const id = req.params.id
+  User.findById({ _id: id })
+    .then(user => res.json(user))
+    .catch(e => {
+      console.log('Error fetching user: ', e)
+      res.status(404).send({ error: 'Malformatted ID'})
+    }) 
+})
+
+// delete specific user
+usersRouter.delete('/:id', (req, res) => {
+  const id = req.params.id
+  User.findByIdAndDelete({ _id: id })
+    .then(() => res.status(204).end())
+    .catch(e => {
+      console.log('Error deleting user: ', e)
+      res.status(404).send({ error: 'Error deleting user.' })
+    })
+})
+
+// create new user
+usersRouter.post('/', async (req, res) => {
+  const { username, password } = req.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  if( !username || !password ){
+    return res.status(400).json({error: 'Missing credentials'})
+  }
+
+  const user = new User({
+    username,
+    passwordHash,
+    workouts: req.body.workouts || [],
+  })
+
+  const savedUser = await user.save()
+
+  res.status(201).json(savedUser)
+})
+
+// update user
+usersRouter.put('/:id', (req, res) => {
+  const id = req.params.id
+  const body = req.body
+
+  User.findById({ _id: id })
+  .then(user => {
+    if(!user){
+      return res.status(404).end()
+    }
+    console.log(user)
+
+    user.username = body.username
+    user.workouts = body.workouts
+
+    return user.save().then((updatedUser) => {
+      res.json(updatedUser)
+    })
+  })
+  .catch(e => {
+    console.log('Error editing user: ', e)
+    res.status(404).send({ error: 'Error editing user.' })
+  })
+})
+
+module.exports = usersRouter
