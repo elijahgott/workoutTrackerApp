e4b8a826637dcfab0ce9bec3960be494c736742e
elijahgott <gottelijah@gmail.com>
2025-09-24 17:20:22 -0500

    feat: added sort button to sort workouts alphabetically

--- frontend/src/components/Workouts.jsx
+++ frontend/src/components/Workouts.jsx
@@ -15,15 +15,35 @@ const Grid = styled.div`
 const Workouts = ({ fetchWorkouts, currentUser, workouts, setWorkouts }) => {
   const [notificationMessage, setNotificationMessage] = useState(null)
 
+  const [sortType, setSortType] = useState('None')
+
+  const sortedWorkouts = workouts.toSorted((a, b) => a.name.localeCompare(b.name))
+
+  const handleChangeSortType = () => {
+    if(sortType === 'None'){
+      setSortType('A-Z')
+    }
+    else{
+      setSortType('None')
+    }
+  }
+
   return(
     <>
       <h1>My Workouts</h1>
+      <label htmlFor='sortBtn'>Sort By:<button id='sortBtn' onClick={handleChangeSortType}>{sortType}</button></label>
       <Notification message={notificationMessage} />
       {
       workouts.length === 0 ? <p>Nothing here...</p>
       :
       <Grid>
-        {workouts.map(w => {
+        {sortType === 'None' 
+        ? workouts.map(w => {
+          return(
+            <Workout key={w.name} workout={w} workouts={workouts} setWorkouts={setWorkouts} currentUser={currentUser} setNotificationMessage={setNotificationMessage} fetchWorkouts={fetchWorkouts} />
+          )
+        })
+        : sortedWorkouts.map(w => {
           return(
             <Workout key={w.name} workout={w} workouts={workouts} setWorkouts={setWorkouts} currentUser={currentUser} setNotificationMessage={setNotificationMessage} fetchWorkouts={fetchWorkouts} />
           )
